c0719136eb6939eded9d3c7f5685a2cda7e79623
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-01 23:45:09 +0530

    star pattern with print

--- Basic/Hello.java
+++ Basic/Hello.java
@@ -2,6 +2,7 @@ package Basic;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("*\n**\n***\n****");
     }
+
 }
