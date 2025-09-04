2a76d0c11a8e508013841721c255e56748e6c26f
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-05 00:45:35 +0530

    IsEven

---
+++ Leetcode/IsEven.java
@@ -0,0 +1,27 @@
+package Leetcode;
+
+import java.util.Scanner;
+
+public class IsEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter number:");
+        int num = sc.nextInt();
+
+        int isPrime = 1;
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1) {
+            System.out.println(String.format("%d is prime number", num));
+        } else {
+            System.out.println("hello");
+            System.out.println(String.format("%d is not prime number", num));
+        }
+    }
+}
