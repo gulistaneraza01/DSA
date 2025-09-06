36f4e67640140faff5514de959b7a350da7ac6ab
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-07 00:14:59 +0530

    star problum

--- Leetcode/HalfPramid180.java
+++ Leetcode/HalfPramid180.java
@@ -2,21 +2,22 @@ package Leetcode;
 
 import java.util.Scanner;
 
-public class HalfPramid {
+public class HalfPramid180 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter number of line to display half Pramid:");
 
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
+        for (int i = 1; i <= n; i++) {
+            for(int k)
+
+            // for (int j = 1; j <= i; j++) {
+            //     System.out.print("*");
+            // }
             System.out.println();
         }
 
         sc.close();
     }
-
 }
--- Leetcode/IvertedHalfPramid.java
+++ Leetcode/IvertedHalfPramid.java
@@ -2,15 +2,15 @@ package Leetcode;
 
 import java.util.Scanner;
 
-public class HalfPramid {
+public class IvertedHalfPramid {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter number of line to display half Pramid:");
 
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
@@ -18,5 +18,4 @@ public class HalfPramid {
 
         sc.close();
     }
-
 }
