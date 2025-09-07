d68fa3487d5a01a57e47e3efe20855c202d48db8
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-07 18:02:10 +0530

    star problum

--- Leetcode/FloyedTriangle.java
+++ Leetcode/FloyedTriangle.java
@@ -2,20 +2,23 @@ package Leetcode;
 
 import java.util.Scanner;
 
-public class InvertedNumberHalfParamid {
+public class FloyedTriangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter number of line to display half Pramid:");
 
         int n = sc.nextInt();
+        int start = 1;
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= n - i; j++) {
-                System.out.print(j + " ");
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(start + " ");
+                start++;
+
             }
             System.out.println();
         }
-
         sc.close();
     }
 }
