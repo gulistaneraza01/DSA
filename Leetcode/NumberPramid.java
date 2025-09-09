a1d7fc8b84b8eb893d1d3ae6ccca466085228d40
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-09 18:23:15 +0530

    number pramide pattren

--- Leetcode/NumberPramid.java
+++ Leetcode/NumberPramid.java
@@ -1,21 +1,21 @@
 package Leetcode;
 
-public class Rhombus {
+public class NumberPramid {
     public static void main(String[] args) {
         int n = 5;
+
         for (int i = 1; i <= n; i++) {
+            // space
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || j == 1 || j == n || i == n) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+
             }
             System.out.println();
+
         }
     }
 }
