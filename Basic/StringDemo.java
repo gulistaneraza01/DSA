96d03754ccd3db8fd58e485bb7805509e3328e9e
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-10 01:44:40 +0530

    string demo

---
+++ Basic/StringDemo.java
@@ -0,0 +1,37 @@
+package Basic;
+
+public class StringDemo {
+    public static void main(String[] args) {
+
+        // int num = 123;
+
+        String numStr = "123";
+
+        // String firstName = "gulistane";
+        // firstName = "raza";
+
+        // String lastName = "raza hello";
+        // String fullName = firstName.concat(lastName);
+        // // split() returns a String array, not a printable string directly
+        // String[] parts = lastName.split(" ");
+
+        // System.out.println(firstName.substring(2, 6));
+
+        // System.out.println(firstName.trim());
+
+        // System.out.println(fullName.charAt(0));
+        // System.out.println(lastName.charAt(3));
+
+        // compareTo returns an int, not a boolean. Check if result is 0 for equality.
+
+        // if (firstName.compareTo(lastName) == 0) {
+        // System.out.println(true);
+        // } else {
+        // System.out.println(false);
+        // }
+
+        // for (int i = 0; i < fullName.length(); i++) {
+        // System.out.println(fullName.charAt(i));
+        // }
+    }
+}
--- Leetcode/DaimondPattern.java
+++ Leetcode/DaimondPattern.java
@@ -10,11 +10,7 @@ public class DaimondPattern {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-
-            for (int j = 2; j <= i; j++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
 
@@ -27,11 +23,7 @@ public class DaimondPattern {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-
-            for (int j = 2; j <= i; j++) {
+            for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
 
