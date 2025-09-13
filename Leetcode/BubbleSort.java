994af37b0905361796d3e98482b89f6ddbc02f40
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-13 20:02:27 +0530

    Insertion sort

--- Leetcode/BubbleSort.java
+++ Leetcode/BubbleSort.java
@@ -6,13 +6,13 @@ public class BubbleSort {
     public static void main(String[] args) {
         int[] nums = { 4, 1, 6, 4, 9, -1, 7, 2 };
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 1; i++) {
 
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                if (nums[i] > nums[j]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
+            for (int j = 0; j < nums.length - i - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
 
             }
---
+++ Leetcode/InsertionSort.java
@@ -0,0 +1,25 @@
+package Leetcode;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = { 4, 3, 1, 5 };
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+
+        }
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
