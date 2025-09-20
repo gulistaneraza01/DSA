c2d0452698d335fbd32bb559fea4aff3f7bbf7a2
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-20 22:39:52 +0530

    backtracking basic

--- Leetcode/TawerOfHonia.java
+++ Leetcode/TawerOfHonia.java
@@ -40,17 +40,78 @@ public class TawerOfHonia {
 
     }
 
+    protected static boolean isSort(int[] nums, int left, int right) {
+        if (nums.length - 1 == right) {
+            return true;
+        }
+
+        if (nums[left] < nums[right]) {
+            return isSort(nums, left + 1, right + 1);
+        } else {
+            return false;
+        }
+
+    }
+
+    // public static StringBuilder newStr = new StringBuilder("");
+    public static String newStr = "";
+
+    protected static void moveAllX(String str, int i, int count) {
+        if (str.length() == i) {
+            for (int j = 0; j < count; j++) {
+                newStr += 'x';
+
+                // newStr.insert(newStr.length(), "x");
+            }
+
+            System.out.println(newStr);
+            return;
+        }
+
+        if (str.charAt(i) == 'x') {
+            moveAllX(str, i + 1, count + 1);
+
+        } else {
+            // newStr.insert(newStr.length(), str.charAt(i));
+            newStr += str.charAt(i);
+            moveAllX(str, i + 1, count);
+        }
+
+    }
+
+    public static boolean[] map = new boolean[32];
+    public static String unique = "";
+
+    protected static void removeDuplicate(String str, int i) {
+        if (str.length() == i) {
+            System.out.println(unique);
+            return;
+        }
+
+        if (map[str.charAt(i) - 'a'] == true) {
+            removeDuplicate(str, i + 1);
+        } else {
+            unique += str.charAt(i);
+            map[str.charAt(i) - 'a'] = true;
+            removeDuplicate(str, i + 1);
+        }
+
+    }
+
     public static void main(String[] args) {
 
-        String name = "razaxa";
-        firstAndLast(name, 0);
-
-        System.out.println(firstIndex);
-        System.out.println(lastIndex);
+        String name = "xxxrazaxa";
+        // firstAndLast(name, 0);
 
         // reverse(name, 0);
 
         // int n = 2;
         // Tower(n, "s", "h", "d");
+
+        // int[] nums = { 1, 5, 6, 7 };
+
+        // System.out.println(isSort(nums, 0, 1));
+        removeDuplicate(name, 0);
+
     }
 }
