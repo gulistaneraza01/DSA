b66917f71b72163ff816cc586bd66e2c293b2585
Gulistane Raza <gulistaneraza01@gmail.com>
2025-09-29 19:41:57 +0530

    hashset

---
+++ Basic/SetBasic.java
@@ -0,0 +1,22 @@
+package Basic;
+
+import java.util.HashSet;
+
+public class SetBasic {
+    public static void main(String[] args) {
+        HashSet<Integer> hash = new HashSet<>();
+
+        hash.add(2);
+        hash.add(3);
+        hash.add(3);
+        hash.add(7);
+        hash.add(5);
+        hash.add(5);
+
+        // for (int item : hash) {
+        // System.out.println(item);
+        // }
+
+    }
+
+}
